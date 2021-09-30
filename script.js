93ce8dc810f02361b08d098282201938f5bca47d
Glitch (glitch-hello-website) <none>
2021-09-30 14:41:04 +0000

    📅💧 Checkpoint ./style.css:32259713/257 ./script.js:32259713/66308

--- script.js
+++ script.js
@@ -3,17 +3,98 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 - Initially the JS writes a message to the console, and rotates a button you can add from the README
 */
 
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic3d1LXRlc3QiLCJhIjoiY2tvdG9mdWM1MDV4OTJ2bzdmdnA5ejEyZyJ9.uuSmjrOs4J48Xek-YT6bgw';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic3d1LXRlc3QiLCJhIjoiY2tvdG9mdWM1MDV4OTJ2bzdmdnA5ejEyZyJ9.uuSmjrOs4J48Xek-YT6bgw";
+const map = new mapboxgl.Map({
+  container: "mapContainer",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [126.951073, 37.54893],
+  zoom: 13
+});
 
-    const map = new mapboxgl.Map({
-      container: 'mapContainer',
-      style: 'mapbox://styles/swu-test/cktnuxsl20nix17qowrvldwms',
-      center: [126.951073, 37.548930],
-      maxZoom: 15.5,
-      minZoom: 10,
-      pitch: 0
-    });
+map.on("load", () => {
+  map.addSource("places", {
+    // This GeoJSON contains features that include an "icon"
+    // property. The value of the "icon" property corresponds
+    // to an image in the Mapbox Streets style's sprite.
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {
+            description:
+              '<strong>Make it Mount Pleasant</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
+            icon: "theatre-15"
+          },
+          geometry: {
+            type: "Point",
+            coordinates: 
+              
+                
+                [126.940016, 37.562377]
+              
+            
+          }
+        },
+        {
+          type: "Feature",
+          properties: {
+            description:
+              '<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a <a href="http://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">Mad Men Season Five Finale Watch Party</a>, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>',
+            icon: "theatre-15"
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [-77.003168, 38.894651]
+          }
+        }
+      ]
+    }
+  });
+  // Add a layer showing the places.
+  map.addLayer({
+    id: "places",
+    type: "symbol",
+    source: "places",
+    layout: {
+      "icon-image": "{icon}",
+      "icon-allow-overlap": true
+    }
+  });
 
-    map.on('load', function () {
-    map.resize();
+  // When a click event occurs on a feature in the places layer, open a popup at the
+  // location of the feature, with description HTML from its properties.
+  map.on("click", "places", e => {
+    // Copy coordinates array.
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const description = e.features[0].properties.description;
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(description)
+      .addTo(map);
+  });
+
+  // Change the cursor to a pointer when the mouse is over the places layer.
+  map.on("mouseenter", "places", () => {
+    map.getCanvas().style.cursor = "pointer";
+  });
+
+  // Change it back to a pointer when it leaves.
+  map.on("mouseleave", "places", () => {
+    map.getCanvas().style.cursor = "";
+  });
+});
+
+map.on("load", function() {
+  map.resize();
 });
