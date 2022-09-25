f868840b3d0134ba03bb0b51f5e0f968d8404226
Glitch (glitch-hello-website) <none>
2022-09-25 17:45:09 +0000

    🍶🏖 Checkpoint ./script.js:32259713/1242

--- script.js
+++ script.js
@@ -25,7 +25,52 @@ map.on("load", () => {
       "fill-color": "rgba(255, 0, 255, 1)" //개체 컬러 바꾸기
     }
   });
+  
+    map.addLayer({
+    id: "test-layer",
+    type: "fill",
+    source: "places",
+    paint: {
+      "fill-color": "rgba(255, 0, 255, 1)" //개체 컬러 바꾸기
+    }
+  });
 
+  
+  map.on('style.load', function () {
+        map.addLayer({
+            id: 'custom_layer',
+            type: 'custom',
+            source: "places",
+            renderingMode: '3d',
+            onAdd: function (map, mbxContext) {
+
+                window.tb = new Threebox(
+                    map,
+                    mbxContext,
+                    { defaultLights: true }
+                );
+
+                var options = {
+                    obj: 'https://cdn.glitch.global/bab878d5-56dd-45eb-906b-767c5be85682/test.gltf?v=1662482246891',
+                    type: 'gltf',
+                    scale: 1,
+                    units: 'meters',
+                    rotation: { x: 90, y: 0, z: 0 } //default rotation
+                }
+
+                tb.loadObj(options, function (model) {
+                    soldier = model.setCoords(origin);
+                    tb.add(soldier);
+                })
+
+            },
+            render: function (gl, matrix) {
+                tb.update();
+            }
+        });
+    })
+  
+  
   
   // 개체를 클릭하면 일어나는 이벤트를 설정하는 영역
   map.on("click", "test-layer", e => {
