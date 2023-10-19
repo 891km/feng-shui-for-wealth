a4070ba8a18090ded9584fa249bce7a730eee6f2
Glitch (glitch-hello-website) <none>
2023-10-19 14:24:24 +0000

    🏐🐵 Checkpoint ./.glitch-assets:55315810/368 ./script.js:55315810/187

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ map.on("load", () => {
   
   // dong_icon
   // https://cdn.glitch.global/1a457f74-eb98-4ed1-8631-b5320b847340/pos.png?v=1695349509014
-  map.loadImage('https://cdn.glitch.global/6866da1d-b241-4b37-a22b-ab00a9127f17/location-pin.png?v=1697723231122', function (error, image) {
+  map.loadImage('https://cdn.glitch.global/6866da1d-b241-4b37-a22b-ab00a9127f17/home.png?v=1697724986889', function (error, image) {
     if (error) throw error;
     map.addImage('dong_icon', image); // 이미지를 맵에 추가합니다.
     
@@ -83,7 +83,7 @@ map.on("load", () => {
         'icon-image': 'dong_icon', // reference the image
         'icon-size': 0.06,
         'icon-anchor': 'bottom',
-        'icon-offset': [0, -3]
+        'icon-offset': [0, -10]
       }
     });
       
